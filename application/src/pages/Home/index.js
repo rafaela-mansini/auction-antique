bfacdeb72841443515c4078c3f759e6ccc319e8b
Rafaela Mansini <rafaela_mansini@hotmail.com>
2021-04-21 15:39:48 -0300

    refactor:  search products by API

--- application/src/pages/Home/index.js
+++ application/src/pages/Home/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Container } from '@material-ui/core';
+import api from 'services/api';
 
 import Layout from 'components/Layout';
 import Search from 'components/Search';
@@ -7,43 +8,17 @@ import Card from 'components/Card';
 // import CardProduct from 'components/CardProduct';
 
 const Home = () => {
-  const products = [
-    {
-      id: 1,
-      name: "Product one",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      initialBid: 15,
-      expireTime: new Date('2021', '04', '21', '12', '27')
-    },
-    {
-      id: 2,
-      name: "Product two",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      initialBid: 15,
-      expireTime: new Date('2021', '04', '21', '12', '27')
-    },
-    {
-      id: 3,
-      name: "Product three",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      initialBid: 15,
-      expireTime: new Date('2021', '04', '21', '12', '27')
-    },
-    {
-      id: 4,
-      name: "Product four",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      initialBid: 15,
-      expireTime: new Date('2021', '04', '21', '12', '27')
-    },
-    {
-      id: 5,
-      name: "Product five",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      initialBid: 15,
-      expireTime: new Date('2021', '04', '21', '12', '27')
-    },
-  ];
+
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await api.get('products');
+      await setProducts(response.data.data);
+    }
+
+    getProducts();
+  }, []);
+
   return(
     <>
       <Layout>
