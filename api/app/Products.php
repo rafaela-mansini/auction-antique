7fd98b0930062bac444ee20d0b3da17d99bb23c2
Rafaela Mansini <rafaela_mansini@hotmail.com>
2021-04-21 18:43:38 -0300

    refactor: remove unused search function to model

--- api/app/Products.php
+++ api/app/Products.php
@@ -7,23 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     protected $fillable = ['name', 'description', 'image', 'initial_bid', 'expired_time'];
-
-    public function search($description, $sortBy){
-        if(empty($description) && empty($sortBy)) {
-            return null;
-        }
-
-        $query = $this->where('description', 'like', "%$description%");
-
-        if(empty($sortBy)){
-            echo 'here1';
-            $query->orderBy('created_at', 'desc');
-        }
-        else{
-            echo 'here2';
-            $query->orderBy('initial_bid', $sortBy);
-        }
-
-        return $query;
-    }
 }
